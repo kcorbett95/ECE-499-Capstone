9fdea8f18106c83a09b31129ccf42d65fe1ac80a
kcorbett95 <55255739+kcorbett95@users.noreply.github.com>
2026-06-21 17:02:29 -0700

    Encoder printing to LCD, Working

---
+++ include/enc.h
@@ -0,0 +1,19 @@
+/*
+/   Encoder Model: AMT102-V
+/   Pinout:
+/       Blue: B-Channel
+/       Red: +5V
+/       Green: A-Channel
+/       Purple: Index Channel
+/       Black: GND
+/   DIP Settings: 0 0 0 0
+/   Resolution (PPR): 2048/Rotation
+/   
+*/
+
+#include <Arduino.h>
+#include <LibPrintf.h>
+
+#define ENC_CHANNEL_A 2
+#define ENC_CHANNEL_B 3
+#define ENC_CHANNEL_INDX 8
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <Arduino.h>
+#include "enc.h"
+#include <LibPrintf.h>
+#include <Encoder.h>
+#include <TMCStepper.h>
 #include <LiquidCrystal.h>
-
 
 //LCD Pin	  Connection	  Uno	        Nano
 ///VSS	    GND	          GND header	GND pin
@@ -16,7 +19,12 @@
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 
+int prevPos = 0;
+
+Encoder myenc(ENC_CHANNEL_A, ENC_CHANNEL_B);
+
 void setup() {
+
     delay(100);         // allow LCD to settle after power-on
     lcd.begin(16, 2);   // 16 columns, 2 rows
     lcd.clear();
@@ -25,9 +33,42 @@ void setup() {
     lcd.print("ViVitro Labs");
 
     lcd.setCursor(0, 1);
+    lcd.print("Capstone 2026");
+
+    delay(2000);
+
+    lcd.setCursor(0, 0);
+    lcd.print("N_Turns: ");
+
+    lcd.setCursor(0, 1);
+    lcd.print("Count: ");
+
+    Serial.begin(9600);
+
+    lcd.setCursor(0, 1);
     lcd.print("Capstone 2026");z
 }
 
 void loop() {
-}
 
+    double newPos, revolutions;
+
+    newPos = myenc.read();
+
+    if(newPos != prevPos){
+        revolutions = newPos/(2048.0*4.0);
+        printf("Count: %f\t Revs: %3.1f\n", newPos, revolutions);
+        
+        char buf[9];
+        lcd.setCursor(8, 0);
+        dtostrf(revolutions, 7, 2, buf);
+        lcd.print(buf);
+
+        lcd.setCursor(7, 1);
+        dtostrf(newPos, 8, 0, buf);
+        lcd.print(buf);
+
+        prevPos = newPos;
+  }
+  
+}
