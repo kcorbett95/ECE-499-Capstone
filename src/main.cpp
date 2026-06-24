6d16430a6ddca693f10f1928da3dadb7fdf751f9
kcorbett95 <55255739+kcorbett95@users.noreply.github.com>
2026-06-24 16:28:32 -0700

    Added Nano Pin Connection Table, DIR pin set to output

--- src/main.cpp
+++ src/main.cpp
@@ -33,6 +33,83 @@
 /*=============================*/
 
 /*========== GLOBALS ==========*/
+/*
+ * =============================================================================
+ * ARDUINO NANO PIN CONNECTION TABLE
+ * =============================================================================
+ *
+ * LCD 16x2 (HD44780, 4-bit mode)
+ * ---------------------------------------------------------------------------
+ *  LCD Pin    Signal           Nano Pin    Notes
+ *  -------    ------           --------    -----
+ *  VSS        GND              GND
+ *  VDD        +5V              5V
+ *  RS         Register Sel.    D12
+ *  RW         Read/Write       GND         Write-only mode
+ *  EN         Enable           D11
+ *  D4         Data 4           D4
+ *  D5         Data 5           D5
+ *  D6         Data 6           D6
+ *  D7         Data 7           D7
+ *  A  (BL+)   Backlight +      5V          Via 220 ohm series resistor
+ *  K  (BL-)   Backlight -      GND
+ *
+ * Encoder: AMT102-V
+ * ---------------------------------------------------------------------------
+ *  Wire       Signal           Nano Pin    Notes
+ *  ----       ------           --------    -----
+ *  Red        +5V              5V
+ *  Black      GND              GND
+ *  Green      A-Channel        D2          INT0
+ *  Blue       B-Channel        D3          INT1
+ *  Purple     Index            NC          Unused
+ *
+ * TMC2209 Stepper Driver #1 (Linear Axis)
+ * ---------------------------------------------------------------------------
+ *  Signal     Nano Pin         Notes
+ *  ------     --------         -----
+ *  STEP       D8
+ *  DIR        D9
+ *  EN         GND              Tied low = always enabled
+ *  CLK        GND              Tied low = internal clock
+ *  VDD        5V               Logic supply
+ *  VM         24V              Motor power supply
+ *  GND        GND              Common with 24V supply GND
+ *
+ * TMC2209 Stepper Driver #2
+ * ---------------------------------------------------------------------------
+ *  Signal     Nano Pin         Notes
+ *  ------     --------         -----
+ *  STEP       D10
+ *  DIR        D13              Shares onboard LED
+ *  EN         GND              Tied low = always enabled
+ *  CLK        GND              Tied low = internal clock
+ *  VDD        5V               Logic supply
+ *  VM         24V              Motor power supply
+ *  GND        GND              Common with 24V supply GND
+ *
+ * End-Stop Switch (NO, 3-wire, 24V)
+ * ---------------------------------------------------------------------------
+ *  Wire       Signal           Connection  Notes
+ *  ----       ------           ----------  -----
+ *  Brown      +24V             24V supply
+ *  Blue       0V / GND         GND         Common with Arduino GND
+ *  Black      NO contact       A2          INPUT_PULLUP; LOW = tripped
+ *
+ * Jog Buttons (momentary NO, one side to GND)
+ * ---------------------------------------------------------------------------
+ *  Button     Nano Pin         Notes
+ *  ------     --------         -----
+ *  Jog CW     A0               INPUT_PULLUP; LOW = pressed
+ *  Jog CCW    A1               INPUT_PULLUP; LOW = pressed
+ *
+ * Reset Button
+ * ---------------------------------------------------------------------------
+ *  RESET pin to GND            Internal pull-up; no I/O pin required
+ *
+ * =============================================================================
+ */
+
 int prevPos = 0;
 long issuedSteps = 0;
 const double STEPS_PER_COUNT = (WIRE_DIAMETER / LINEAR_RES) / (RESOLUTION * 4.0);
@@ -41,7 +118,6 @@ const double STEPS_PER_COUNT = (WIRE_DIAMETER / LINEAR_RES) / (RESOLUTION * 4.0)
 /*========== CLASSES ==========*/
 stepper linearStepper(STEP_PIN,DIR_PIN);
 Encoder myenc(ENC_CHANNEL_A, ENC_CHANNEL_B);
-/*=============================*/
 
 // LiquidCrystal(rs, en, d4, d5, d6, d7)
 LiquidCrystal lcd(12, 11, 4, 5, 6, 7);
